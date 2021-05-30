a10c5cc18c96afb6615bda99cf8dd5505963fadc
Vestry <vestry.odessa@gmail.com>
2021-05-30 13:56:44 +0300

    feat: add jsdoc for services

--- src/resources/boards/service.js
+++ src/resources/boards/service.js
@@ -7,14 +7,40 @@ const {
 } = require('./memory.repository');
 const { clearTasksByBoardId } = require('../tasks/service');
 
+/**
+ * Return result of execution 'getAllBoards' function
+ * @returns {Promise<{columns: Column[], id: string, title: string}[]>}
+ */
 const getAll = async() => getAllBoards();
 
+/**
+ * Return result of execution 'getBoardById' function
+ * @param {string} boardId
+ * @returns {Promise<{columns: Column[], id: string, title: string}|null>}
+ */
 const getById = async boardId => getBoardById(boardId);
 
+/**
+ * Return result of execution 'createBoard' function
+ * @param {{id: string, title: string, columns: Array.<Column> | Array}} payload
+ * @returns {Promise<{columns: Column[], id: string, title: string}>}
+ */
 const create = async payload => createBoard(payload);
 
+/**
+ * Return result of execution 'updateBoard' function
+ * @param {string} boardId
+ * @param {{id: string, title: string, columns: Array.<Column> | Array}} payload
+ * @returns {Promise<{columns: Column[], id: string, title: string} | null>}
+ */
 const update = async (boardId, payload) => updateBoard(boardId, payload);
 
+/**
+ * First we execute 'clearTasksByBoardId' function. If success - then delete board
+ * by execution 'deleteById' function
+ * @param {string} boardId
+ * @returns {Promise<null | string>}
+ */
 const remove = async boardId => {
   const cleared = await clearTasksByBoardId(boardId);
   if (!cleared.length) {
--- src/resources/users/service.js
+++ src/resources/users/service.js
@@ -7,14 +7,40 @@ const {
 } = require('./memory.repository');
 const { updateTasks } = require('../tasks/service');
 
+/**
+ * Return result of execution 'getAllUsers' function
+ * @returns {Promise<{id: string, name: string, login: string}[]>}
+ */
 const getAll = async () => getAllUsers();
 
+/**
+ * Return result of execution 'getUserById' function
+ * @param {string} userId
+ * @returns {Promise<Object.<User>|null>}
+ */
 const getById = async userId => getUserById(userId);
 
+/**
+ * Return result of execution 'updateUserInfo' function
+ * @param {string} userId
+ * @param {{name: string, login: string, password: string}} payload
+ * @returns {Promise<null|Object.<User>>}
+ */
 const update = async ({ userId, payload }) => updateUserInfo({ userId, payload });
 
+/**
+ * Return result of execution 'createUser' function
+ * @param {{name: string, login: string, password: string}} payload
+ * @returns {Promise<Object.<User>>}
+ */
 const create = async payload => createUser(payload);
 
+/**
+ * First we execute 'updateTasks' function - set appropriate field yo null,
+ * then we execute 'removeUserById' and delete user.
+ * @param {string} userId
+ * @returns {Promise<null|Object.<User>>}
+ */
 const remove = async userId => {
   await updateTasks(userId);
   return removeUserById(userId);
