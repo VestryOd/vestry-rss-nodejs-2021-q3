c78b8979c53a3df67d161aaca6781d94adbb4c9f
Vestry <vestry.odessa@gmail.com>
2021-06-08 22:58:41 +0300

    feat: finish errors logging

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ import path from 'path';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
 
+import {
+  errorHandler,
+  eventLogger,
+  onUncaughtException,
+  onUnhandledPromiseRejection,
+} from './middleware/error-hadler';
 import boardsRouter from './resources/boards/router';
 import userRouter from './resources/users/router';
 
@@ -21,7 +27,21 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+app.use(eventLogger);
 app.use('/users', userRouter);
 app.use('/boards', boardsRouter);
+app.use(errorHandler);
+
+process.on('uncaughtException', (error: Error) => {
+  onUncaughtException(error);
+});
+
+process.on('unhandledRejection', (error: Error) => {
+  onUnhandledPromiseRejection(error);
+});
+
+// throw Error('oops');
+
+// Promise.reject(new Error('Oops!'));
 
 export default app;
